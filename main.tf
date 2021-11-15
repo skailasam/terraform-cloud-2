138e8a967c028a263a8e4ade4c7a92004166139a
Obi Okoye <obinnao@outlook.com>
2021-11-15 23:25:26 +0000

    update

--- main.tf
+++ main.tf
@@ -3,28 +3,11 @@ provider "aws" {
 }
 
 # Create VPC
-resource "aws_vpc" "zcloud" {
+resource "aws_vpc" "main" {
   cidr_block                     = "172.16.0.0/16"
   enable_dns_support             = "true"
   enable_dns_hostnames           = "true"
   enable_classiclink             = "false"
   enable_classiclink_dns_support = "false"
-}
-
-# Create public subnets1
-    resource "aws_subnet" "public1" {
-    vpc_id                     = aws_vpc.main.id
-    cidr_block                 = "172.16.0.0/24"
-    map_public_ip_on_launch    = true
-    availability_zone          = "us-east-1a"
-
-}
-
-# Create public subnet2
-    resource "aws_subnet" "public2" {
-    vpc_id                     = aws_vpc.main.id
-    cidr_block                 = "172.16.1.0/24"
-    map_public_ip_on_launch    = true
-    availability_zone          = "us-east-1b"
 }
 
